fd21f2fe6437fb34ce44a8e808ecfa2157f76bb8
EreblinaZ <zereblina@gmail.com>
2021-09-10 00:56:42 +0200

    dizajn

--- js/faq.js
+++ js/faq.js
@@ -1,15 +1,16 @@
+'use strict';
+
 const faqItemHead = document.querySelectorAll('.faq-item-head');
 
-
 faqItemHead.forEach((head, index) => {
-    head.addEventListener('click', () => {
-        let icon = head.querySelector('.faq-item-icon').firstElementChild;
-        if(icon.className == "fa fa-chevron-down"){
-            head.nextElementSibling.classList.add('show-para');
-            icon.className = "fa fa-chevron-up";
-        } else if(icon.className == "fa fa-chevron-up"){
-            head.nextElementSibling.classList.remove('show-para');
-            icon.className = "fa fa-chevron-down";
-        }
-    });
+  head.addEventListener('click', () => {
+    let icon = head.querySelector('.faq-item-icon').firstElementChild;
+    if (icon.className == 'fa fa-chevron-down') {
+      head.nextElementSibling.classList.add('show-para');
+      icon.className = 'fa fa-chevron-up';
+    } else if (icon.className == 'fa fa-chevron-up') {
+      head.nextElementSibling.classList.remove('show-para');
+      icon.className = 'fa fa-chevron-down';
+    }
+  });
 });
