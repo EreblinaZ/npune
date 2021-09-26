c0f8361b97cfdf085147c4bd840a962460a4c2c5
EreblinaZ <zereblina@gmail.com>
2021-09-27 00:23:04 +0200

    Konfigurimi i ngjyrave me tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,4 +8,26 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
+
+// tailwind.config.js
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      gray: colors.coolGray,
+      red: colors.rose,
+      yellow: colors.amber,
+      green: colors.emerald,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      purple: colors.violet,
+      pink: colors.pink,
+      black: colors.black,
+      white: colors.white,
+    },
+  },
+};
